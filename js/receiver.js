2b5996b8345c1cc132a2e74beee3001f4f918457
nstrain <nstrain15@gmail.com>
2021-10-20 17:48:59 -0400

    remove errorchecking because i can

--- js/receiver.js
+++ js/receiver.js
@@ -32,6 +32,7 @@ playerManager.setMessageInterceptor(
     castDebugLogger.debug(LOG_RECEIVER_TAG,
       `loadRequestData: ${JSON.stringify(loadRequestData)}`);
 	      // If the loadRequestData is incomplete return an error message
+	setTest(loadRequestData.media.customData.test);
     if (loadRequestData || loadRequestData.media || loadRequestData.media.customData || loadRequestData.media.customData.test) {
       setTest(loadRequestData.media.customData.test);
     }
